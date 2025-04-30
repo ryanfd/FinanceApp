2eb7da0b5b8f4e8a9f5f5cd1a26125ff72136deb
ryanfd <rdonnelly98@gmail.com>
2025-04-30 09:48:38 -0700

    typo in dummy card data

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -6,7 +6,7 @@ interface Props {}
 const CardList : React.FC<Props> = (props: Props) : JSX.Element => {
   return (
     <div className='CardList'>
-        <Card companyName='Apple' ticker='APPL' price={110} />
+        <Card companyName='Apple' ticker='AAPL' price={110} />
         <Card companyName='Microsoft' ticker='MSFT' price={200} />
         <Card companyName='Tesla' ticker='TSLA' price={300} />
     </div>
