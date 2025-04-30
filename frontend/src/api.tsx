6937003e4d3a27df94fe2f358254186ed602cd48
ryanfd <rdonnelly98@gmail.com>
2025-04-30 09:46:53 -0700

    get api data from search

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -5,27 +5,33 @@ interface SearchResponse {
     data: CompanySearch[];
 }
 
-export const searchCompanies = async(ticker: string) => {
-    const data = await axios.get<SearchResponse>(
-        `https://financialmodelingprep.com/api/v3/search?query=${ticker}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+export const searchCompanies = async(query: string) : Promise<Axios.AxiosXHR<SearchResponse> | string> => {
+    let data = await axios.get<SearchResponse>(
+        `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
     )
     .catch(function (error) {
+        let errorResult : string;
+
         if (error.response) {
             // server responded with a status code that falls out of the range of 2xx
             console.log(error.response.data);
             console.log(error.response.status);
             console.log(error.response.headers);
+            errorResult = "An error has occured.";
         }
         else if (error.request) {
             // no response was received
             // `error.request` is an instance of XMLHttpRequest in the browser 
             // and an instance of http.ClientRequest in node.js
             console.log(error.request);
+            errorResult = "An error has occured.";
         }
         else {
             console.log("unexpected error:", error);
-            return "An unexpected error has occured."
+            errorResult = "An unexpected error has occured.";
         }
+
+        return errorResult;
     });
 
     return data;
