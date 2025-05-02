1553e323a2ce80fa81271ace1c4a50de6381ab30
ryanfd <rdonnelly98@gmail.com>
2025-05-02 10:39:12 -0700

    Balance Sheet w/ dummy data

--- frontend/src/Components/Sidebar/Sidebar.tsx
+++ frontend/src/Components/Sidebar/Sidebar.tsx
@@ -16,11 +16,11 @@ const Sidebar = (props: Props) => {
                     <Link to="company-profile" className='flex md:min-w-full text-blueGray-500 text-medium uppercase font-bold block pt-1 pb-4 no-underline' >
                         <h6 className='ml-3'>Company Profile</h6>
                     </Link>
-                    <Link to="balance-sheet" className='flex md:min-w-full text-blueGray-500 text-medium uppercase font-bold block pt-1 pb-4 no-underline' >
-                        <h6 className='ml-3'>Balance Sheet</h6>
-                    </Link>
                     <Link to="income-statement" className='flex md:min-w-full text-blueGray-500 text-medium uppercase font-bold block pt-1 pb-4 no-underline' >
                         <h6 className='ml-3'>Income Statement</h6>
+                    </Link>
+                    <Link to="balance-sheet" className='flex md:min-w-full text-blueGray-500 text-medium uppercase font-bold block pt-1 pb-4 no-underline' >
+                        <h6 className='ml-3'>Balance Sheet</h6>
                     </Link>
                 </div>
             </div>
