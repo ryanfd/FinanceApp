370c1bc8ab5319e7b2bc3c55c9b6fe0a816e1047
ryanfd <rdonnelly98@gmail.com>
2025-05-05 15:58:18 -0700

    DTO setup

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationBDContext _context;
+
+        public StockController(ApplicationBDContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] // read
+        public IActionResult GetAll() 
+        {
+            var stocks = _context.Stock.ToList()
+            .Select(s => s.ToStockDto());
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) 
+        {
+            var stock = _context.Stock.Find(id);
+
+
+            if (stock == null) {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+    }
+}
