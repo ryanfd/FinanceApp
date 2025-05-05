762656e2f1fbd1dc9c5ac936e22c299f21f056ab
ryanfd <rdonnelly98@gmail.com>
2025-05-05 11:05:02 -0700

    Updated setup, running with swagger

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae704d0f0a046d52ac254fe29744edb2e2407ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b5a652dd1be8ede1cb52c24754c84ab967fe0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
