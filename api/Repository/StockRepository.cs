9059a9d425349fcde3012af64d2dc80119a7ce28
ryanfd <rdonnelly98@gmail.com>
2025-05-07 09:04:39 -0700

    Stock GetAll() sorting by symbol

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -53,6 +53,14 @@ namespace api.Repository
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
 
+            if (!string.IsNullOrWhiteSpace(query.SortBy)) {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase)) {
+                    stocks = query.IsDescending 
+                        ? stocks.OrderByDescending(s => s.Symbol) 
+                        : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
