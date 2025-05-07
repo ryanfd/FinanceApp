fb29a6f88b154743b756e8650bcc2333101d2276
ryanfd <rdonnelly98@gmail.com>
2025-05-07 08:32:55 -0700

    Stock GetAll() query filtering/search

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -25,13 +26,13 @@ namespace api.Controllers
         }
 
         [HttpGet] // read
-        public async Task<IActionResult> GetAll() 
+        public async Task<IActionResult> GetAll([FromQuery] QueryObject query) 
         {
             if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
 
-            var stocks = await _stockRepo.GetAllAsync();
+            var stocks = await _stockRepo.GetAllAsync(query);
             var stockDto = stocks.Select(s => s.ToStockDto());
 
             return Ok(stocks);
@@ -90,7 +91,7 @@ namespace api.Controllers
             if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
-            
+
             var stockModel = await _stockRepo.DeleteAsync(id);
 
             if (stockModel == null) {
