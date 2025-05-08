50be41074bdf3cc13c64acc7f7dc0c6df57b453b
ryanfd <rdonnelly98@gmail.com>
2025-05-08 15:14:19 -0700

    React Context Auth JWT

---
+++ frontend/src/Helpers/ErrorHandler.tsx
@@ -0,0 +1,27 @@
+import { toast } from "react-toastify";
+
+export const handleError = (error: any) => {
+    var err = error.response;
+
+    if (Array.isArray(err?.data.errors)) {
+        for (let val of err?.data.errors) {
+            toast.warning(val.desc);
+        }
+    }
+    else if (typeof err?.data.errors == 'object') {
+        for (let e in err?.data.errors) {
+            toast.warning(err.data.errors[e][0]);
+        }
+    }
+    else if (err?.data) {
+        toast.warning(err.data);
+    }
+
+    else if (err?.status == 401) {
+        toast.warning("Please login");
+        window.history.pushState({}, "Login Page", "/login");
+    }
+    else if (err) {
+        toast.warning(err?.data);
+    }
+};
