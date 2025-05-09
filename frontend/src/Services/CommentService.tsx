6db622251c342efcfbe1ece5a0703fe08836689d
ryanfd <rdonnelly98@gmail.com>
2025-05-09 10:47:30 -0700

    Comment Box

---
+++ frontend/src/Services/CommentService.tsx
@@ -0,0 +1,19 @@
+import axios from "axios"
+import { CommentPost } from "../Models/Comment";
+import { handleError } from "../Helpers/ErrorHandler";
+
+const api = "http://localhost:5167/api/comment/"
+
+export const commentPostApi = async (title: string, content: string, symbol: string) => {
+    try {
+        const data = await axios.post<CommentPost>(api + `${symbol}`, {
+            title: title,
+            content: content
+        });
+
+        return data;
+    }
+    catch (error) {
+        handleError(error);
+    }
+}
