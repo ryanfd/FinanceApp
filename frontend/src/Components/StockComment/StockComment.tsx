b3883f15d58f2061b01babc40411fdd0b3465483
ryanfd <rdonnelly98@gmail.com>
2025-05-09 11:07:22 -0700

    Comment List

--- frontend/src/Components/StockComment/StockComment.tsx
+++ frontend/src/Components/StockComment/StockComment.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import StockCommentForm from './StockCommentForm/StockCommentForm'
-import { commentPostApi } from '../../Services/CommentService'
+import { commentGetApi, commentPostApi } from '../../Services/CommentService'
 import { toast } from 'react-toastify'
+import { CommentGet } from '../../Models/Comment'
+import Spinner from '../Spinner/Spinner'
+import StockCommentList from '../StockCommentList/StockCommentList'
 
 type Props = {
     stockSymbol: string
@@ -13,20 +16,41 @@ type CommentFormInputs = {
 }
 
 const StockComment = ({stockSymbol}: Props) => {
+    const [comments, setComments] = useState<CommentGet[] | null>(null);
+    const [loading, setLoading] = useState<boolean>();
+
+    useEffect(() => {
+        getComments();
+    }, []);
+    
     const handleComment = (e: CommentFormInputs) => {
         commentPostApi(e.title, e.content, stockSymbol)
             .then((res) => {
                 if (res) {
                     toast.success("Comment created successfully!");
+                    getComments();
                 }
             }).catch((e) => {
                 toast.warning(e);
             });
     }
 
-  return (
-    <StockCommentForm symbol={stockSymbol} handleComment={handleComment} />
-  )
-}
+    const getComments = () => {
+        setLoading(true);
+        commentGetApi(stockSymbol)
+            .then((res) => {
+                setLoading(false);
+                setComments(res?.data!);
+            });
+    };
 
-export default StockComment
+  return (
+    <div className="flex flex-col">
+        {loading ? <Spinner /> : <StockCommentList comments={comments!} />}
+        <StockCommentForm symbol={stockSymbol} handleComment={handleComment} />
+    </div>
+    
+  )
+};
+
+export default StockComment;
